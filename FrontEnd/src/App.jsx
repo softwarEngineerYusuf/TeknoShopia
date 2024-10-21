8365e273a2d7107475b2ba3a6c5a32bc4f1d06c4
softwareYusuf <112049106+softwarEngineerYusuf@users.noreply.github.com>
2024-10-21 13:05:53 +0300

    react project was created with vite and tailwind

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline ">
+    Merhaba ARİF ve BÜŞRA
+  </h1>
+  </>
+  )
+}
+
+export default App
