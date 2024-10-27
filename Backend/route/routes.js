0e8e5f5e1fafe01d2e9d74393ec895f0431da8ff
softwareYusuf <112049106+softwarEngineerYusuf@users.noreply.github.com>
2024-10-27 19:35:19 +0300

    APIs of reviews had been created.

--- Backend/route/routes.js
+++ Backend/route/routes.js
@@ -4,7 +4,8 @@ const userRouter =require("./userRouter.js")
 const category=require("./category.js")
 const brand=require("./brand.js")
 const address=require("./address.js")
-const product=require("./product.js")
+const product=require("./product.js");
+const review=require("./review.js")
 
 
 const base = "/api" 
@@ -14,6 +15,7 @@ router.use(`${base}/user`, userRouter); //localhost:3000/api/user
 router.use(`${base}/category`, category); //localhost:3000/api/category
 router.use(`${base}/brand`, brand); //localhost:3000/api/category
 router.use(`${base}/address`, address); //localhost:3000/api/address
-router.use(`${base}/product`, product); //localhost:3000/api/address
+router.use(`${base}/product`, product); //localhost:3000/api/product
+router.use(`${base}/review`, review); //localhost:3000/api/review
 
 module.exports=router;
