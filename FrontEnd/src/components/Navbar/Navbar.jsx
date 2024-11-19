78f78dc5319126a6bb37d46982ed054a27277eda
arifhalitarslan <arifhalitarslan0@gmail.com>
2024-11-19 16:45:13 +0300

    Navbar added

--- FrontEnd/src/components/Navbar/Navbar.jsx
+++ FrontEnd/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,32 @@
+import React from 'react';
+import { FaShoppingCart, FaUser, FaSearch } from 'react-icons/fa'; // İkonları içe aktar
+import '../Navbar/Navbar.css';
+import  logo from '../../assets/TeknoShopiaLogo.png';
+
 function Navbar() {
-  return <div> </div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" className="logo-image" /> {/* Logo resmini buraya ekleyin */}
+      </div>
+      
+      <div className="navbar-search">
+        <input type="text" placeholder="Search" className="search-input" />
+        <FaSearch className="search-icon" /> {/* Arama ikonu */}
+      </div>
+      
+      <div className="navbar-buttons">
+        <button className="navbar-button">
+          <FaUser className="icon" /> {/* Giriş Yap ikonu */}
+          Login
+        </button>
+        <button className="navbar-button">
+          <FaShoppingCart className="icon" /> {/* Sepetim ikonu */}
+          Basket
+        </button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar;
+export default Navbar;
