7151e0dc41ec43bf13f6a721b7ec46ee14874199
arifhalitarslan <arifhalitarslan0@gmail.com>
2025-02-05 13:29:35 +0300

    HomePage design is loading...

---
+++ FrontEnd/src/components/Navbar2/Navbar2.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar2.css";
+import Chatbot from "../../assets/chatbot.png";
+
+
+function Navbar2() {
+  return (
+    <div className="Navbar2All">
+      <div className="Navbar2Links">
+      <a href="">TV</a>
+      <a href="">Telephone</a>
+      <a href="">Tablet</a>
+      <a href="">Smart Watch</a>
+      <a href="">Camera</a>
+      <a href="">Speaker</a>
+      <a href="">Printer</a>
+      <a href="">Game Console</a>
+      <a href="">Game Chair</a>
+      <a href="">Earphones </a>
+      <a href="">Bluetooth Headset</a>
+      <a href="">
+      <img style={{width:'50px',height:'50px'}} src={Chatbot} alt="Chatbot" />
+      </a>
+    </div>
+    </div>
+  );
+}
+
+export default Navbar2;
--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -1,127 +1,121 @@
-  import React from "react";
-  import "bootstrap/dist/css/bootstrap.min.css";
-  import "bootstrap/dist/js/bootstrap.bundle.min.js";
-  import AdCarousel from "../../components/AdCorousel/AdCorousel.jsx";
-  import DiscountsCarousel from "../../components/DiscountsCorousel/DiscountsCorousel.jsx";
-  import BestSellersCorousel from "../../components/BestSellersCorousel/BestSellersCorousel.jsx";
-  import TopPicksCorousel from "../../components/TopPicksCorousel/TopPicksCorousel.jsx";
-  import "../Home/Home.css";
+import React from "react";
+import "../Home/Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import AdCarousel from "../../components/AdCorousel/AdCorousel.jsx";
+import DiscountsCarousel from "../../components/DiscountsCorousel/DiscountsCorousel.jsx";
+import Navbar2 from "../../components/Navbar2/Navbar2.jsx";
+import TopPicks from "../../components/TopPicks/TopPicks.jsx";
 
-  function Home() {
-    const images = [
-      "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
-      "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
-      "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
-    ];
+function Home() {
+  const images = [
+    "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
+    "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
+    "https://cdn.vatanbilgisayar.com/Upload/BANNER//0banner/2024/011/apple-iphone13-1-11-24-web.jpg",
+  ];
 
-    const cardsData = [
-      {
-        id: 1,
-        title: "Apple iPhone 13 128GB",
-        price: "500$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/129885-1_large.jpg",
-      },
-      {
-        id: 2,
-        title: "Apple iPhone 12 64GB",
-        price: "400$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_small.jpg",
-      },
-      {
-        id: 3,
-        title: "Apple iPhone 11 64GB",
-        price: "300$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-1-1_small.jpg",
-      },
-      {
-        id: 4,
-        title: "Apple iPhone 14 128GB",
-        price: "600$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/135150-1-10_small.jpg",
-      },
-      {
-        id: 5,
-        title: "Apple iPhone 15 128GB",
-        price: "700$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/140315-1-1_small.jpg",
-      },
-      {
-        id: 6,
-        title: "Apple iPhone 16 128GB",
-        price: "800$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/1-246_small.jpg",
-      },
-      {
-        id: 7,
-        title: "Samsung S24 256GB",
-        price: "500$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/1-116_small.jpg",
-      },
-      {
-        id: 8,
-        title: "Samsung S24+ 256GBGB",
-        price: "550$",
-        discountedPrice: "450$",
-        imgSrc:
-          "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/1-113_small.jpg",
-      },
-    ];
+  const cardsData = [
+    {
+      id: 1,
+      title: "Apple iPhone 13 128GB",
+      price: "500$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/129885-1_large.jpg",
+    },
+    {
+      id: 2,
+      title: "Apple iPhone 12 64GB",
+      price: "400$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/110622-1-1_small.jpg",
+    },
+    {
+      id: 3,
+      title: "Apple iPhone 11 64GB",
+      price: "300$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-1-1_small.jpg",
+    },
+    {
+      id: 4,
+      title: "Apple iPhone 14 128GB",
+      price: "600$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/135150-1-10_small.jpg",
+    },
+    {
+      id: 5,
+      title: "Apple iPhone 15 128GB",
+      price: "700$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/140315-1-1_small.jpg",
+    },
+    {
+      id: 6,
+      title: "Apple iPhone 16 128GB",
+      price: "800$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/1-246_small.jpg",
+    },
+    {
+      id: 7,
+      title: "Samsung S24 256GB",
+      price: "500$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/1-116_small.jpg",
+    },
+    {
+      id: 8,
+      title: "Samsung S24+ 256GBGB",
+      price: "550$",
+      discountedPrice: "450$",
+      imgSrc:
+        "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/1-113_small.jpg",
+    },
+  ];
 
-    // Kartları 4'erli gruplar halinde bölen fonksiyon
-    const chunkArray = (array, chunkSize) => {
-      const results = [];
-      for (let i = 0; i < array.length; i += chunkSize) {
-        results.push(array.slice(i, i + chunkSize));
-      }
-      return results;
-    };
+  // Kartları 4'erli gruplar halinde bölen fonksiyon
+  const chunkArray = (array, chunkSize) => {
+    const results = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      results.push(array.slice(i, i + chunkSize));
+    }
+    return results;
+  };
 
-    const cardGroups = chunkArray(cardsData, 4); // Her bir `carousel-item` içinde 4 kart olacak
+  const cardGroups = chunkArray(cardsData, 4); // Her bir `carousel-item` içinde 4 kart olacak
 
-    return (
-      <div>
-        <div className="allMainHomePage">
-          {/* Reklam Carousel */}
-          <div className="corouselOnHomePage">
-            <AdCarousel images={images} />
-          </div>
-
-          {/* Discounts Slider */}
-          <div className="DiscountsTextOnHomePage">
-            <p>Discounts</p>
-          </div>
-          <DiscountsCarousel cardGroups={cardGroups} />
-
-          {/* Best Sellers Slider */}
-          <div className="DiscountsTextOnHomePage">
-            <p>Best Sellers</p>
-          </div>
-          <BestSellersCorousel cardGroups={cardGroups} />
-        
-        
-          {/* Top Picks Slider */}
-          <div className="DiscountsTextOnHomePage">
-            <p>Top Picks</p>
-          </div>
-          <TopPicksCorousel cardGroups={cardGroups} />
-
-        </div>
+  return (
+    <div>
+      <div className="navbar2Home">
+        <Navbar2 />
       </div>
-    );
-  }
 
-  export default Home;
+      <div className="allMainHomePage">
+        {/* Reklam Carousel */}
+        <div className="corouselOnHomePage">
+          <AdCarousel images={images} />
+        </div>
+
+        <div className="TopPicksOnHomePage">
+          <TopPicks />
+        </div>
+
+        {/* Discounts Slider */}
+        <div className="DiscountsTextOnHomePage">
+          <p>Discounts</p>
+        </div>
+        <DiscountsCarousel cardGroups={cardGroups} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
